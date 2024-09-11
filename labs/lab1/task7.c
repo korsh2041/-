14817462cafc8fd07d6a298103b07377eea06bf9
korsh2041 <akorshunov2005@gmail.com>
2024-09-11 17:29:57 +0300

    Update task7.c

--- labs/lab1/task7.c
+++ labs/lab1/task7.c
@@ -1,1 +1,12 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+    FILE *file;
+    const char *filename = "text.txt";
+    char *str = "привет";
+    file = fopen(filename, "a");
+    fputs(str, file);
+    fclose(file);
+    printf("Записано");
+    return 0;
+}
