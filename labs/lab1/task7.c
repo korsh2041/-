888805deb3c7ef68aa31a80248cd153d75aeccee
korsh2041 <akorshunov2005@gmail.com>
2024-09-17 09:23:08 +0300

    Update task7.c

--- labs/lab1/task7.c
+++ labs/lab1/task7.c
@@ -10,3 +10,34 @@ int main() {
     printf("Записано");
     return 0;
 }
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+    char line[256];
+    
+    FILE *file1, *file2;
+    const char *text = "text.txt";
+    char *str = "привет";
+    
+    file1 = fopen(text,"r");
+    printf("Записано до:\n");
+    
+    do {
+        printf("%s",line);
+    }while(fgets(line,256,file1)!=NULL);
+    
+    fclose(file1);
+    
+    // file2 = fopen(text, "a");
+    
+    // fputs(str, file2);
+    
+    // printf("\n Записано после:\n");
+    // while(fgets(line,256,file2)!=NULL){
+    //     printf("%s",line);
+    // }
+    
+    fclose(file2);
+    
+    return 0;
+}
