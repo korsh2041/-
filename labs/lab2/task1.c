0ff0bbc41c511eb1cf8216b332054eb7479a9993
korsh2041 <akorshunov2005@gmail.com>
2024-09-17 16:20:24 +0300

    Update task1.c

--- labs/lab2/task1.c
+++ labs/lab2/task1.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+int main() {
+    int arr[10];
+    
+    srand(time(NULL));
+    
+    for (int i = 0; i < 10; i++) {
+        arr[i] = rand() % 101; 
+    }
+    
+    printf("Сгенерированный массив:\n");
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", arr[i]);
+    }
+    
+    printf("\n");
+
+    return 0;
+}
