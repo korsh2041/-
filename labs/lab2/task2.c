87c3a45828996a1ec8a0903fb0f77210967782ee
korsh2041 <akorshunov2005@gmail.com>
2024-09-17 16:57:23 +0300

    Update task2.c

--- labs/lab2/task2.c
+++ labs/lab2/task2.c
@@ -1,1 +1,33 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int arr[10];
+    int i;
+    srand(time(NULL));
+    
+    for (int i = 0; i < 10; i++) {
+        arr[i] = rand() % 101; 
+    }
+    
+    printf("Сгенерированный массив:\n");
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", arr[i]);
+    
+    }
+    
+    printf("\n");
+    
+    char text[] ="text1.txt";
+    FILE *file = fopen(text,"w");
+    for (int i = 9; i >= 0; i--) {
+        fprintf(file, "%d ", arr[i]);
+    }
+    
+    fclose(file);
+    
+    printf("\nЗаписано в файл");
+    return 0;
+}
