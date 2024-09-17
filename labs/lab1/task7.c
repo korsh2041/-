cfcfca050061f39a77d68d0bdd16e22f5de84217
korsh2041 <akorshunov2005@gmail.com>
2024-09-17 15:25:47 +0300

    Update task7.c

--- labs/lab1/task7.c
+++ labs/lab1/task7.c
@@ -1,43 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main() {
-    FILE *file;
-    const char *text = "text.txt";
-    char *str = "привет";
-    file = fopen(text, "a");
-    fputs(str, file);
-    fclose(file);
-    printf("Записано");
-    return 0;
-}
-#include <stdio.h>
-#include <stdlib.h>
-int main() {
     char line[256];
-    
-    FILE *file1, *file2;
+
     const char *text = "text.txt";
     char *str = "привет";
-    
-    file1 = fopen(text,"r");
-    printf("Записано до:\n");
-    
-    do {
-        printf("%s",line);
-    }while(fgets(line,256,file1)!=NULL);
-    
-    fclose(file1);
-    
-    // file2 = fopen(text, "a");
-    
-    // fputs(str, file2);
-    
-    // printf("\n Записано после:\n");
-    // while(fgets(line,256,file2)!=NULL){
-    //     printf("%s",line);
-    // }
-    
+    printf("Содержимое файла:\n");
+    FILE *file1 = fopen(text, "r");
+    if(file1)
+    {
+        while((fgets(line, 256, file1))!=NULL)
+        {
+            printf("%s", line);
+        }
+        
+        fclose(file1);
+    } 
+    FILE *file2 = fopen(text, "a");
+    fputs(str,file2);
+    printf("\n");
     fclose(file2);
+    
+    printf("Содержимое файла после записи:\n");
+    FILE *file3 = fopen(text,"r");
+    if(file3)
+    {
+        while((fgets(line, 256, file3))!=NULL)
+        {
+            printf("%s", line);
+        }
+        
+        fclose(file3);
+    } 
     
     return 0;
 }
