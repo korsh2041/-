c5d458b8475f23521ee88cee571dee2c32a73fff
korsh2041 <akorshunov2005@gmail.com>
2024-09-24 08:40:37 +0300

    Update task6.c

--- labs/lab1/task6.c
+++ labs/lab1/task6.c
@@ -1,23 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-#define MAX_LINE_LENGTH 256
+#define N 1024
 
 int main() {
-    FILE *file1, *file2;
-    char line[MAX_LINE_LENGTH];
+    FILE *file1, *file2, *file3;
+    char line[N];
     int sch = 0;
-
+    printf("Содержимое файл до:\n");
     file1 = fopen("text.txt", "r");
-  
+    
+    if(file1)
+    {
+        while(fgets(line, sizeof(line), file1) != NULL)
+        {
+            printf("%s", line);
+        }
+    
+        fclose(file1);
+    } 
+    file1 = fopen("text.txt", "r");
     file2 = fopen("text1.txt", "w");
  
     while (fgets(line, sizeof(line), file1) != NULL && sch < 3) {
         fputs(line, file2);
         sch++;
     }
-
-    fclose(file1);
     fclose(file2);
+    printf("\n");
+    file2 = fopen("text1.txt", "r");
+    printf("Содержимое файла в файл записи:\n");
+    
+    if(file2)
+    {
+        while(fgets(line, sizeof(line), file2) != NULL)
+        {
+            printf("%s", line);
+        }
+        fclose(file2);
+    } 
+    
+    fclose(file1);
+    
     return 0;
 }
