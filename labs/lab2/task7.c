84e951cc09a4f0f1efae6833208546683867735e
korsh2041 <akorshunov2005@gmail.com>
2024-09-26 19:01:15 +0300

    Update task7.c

--- labs/lab2/task7.c
+++ labs/lab2/task7.c
@@ -1,1 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 
+#define MAX 100
+
+typedef struct {
+    int items[MAX];
+    int top;
+} Stack;
+
+void initStack(Stack* s) {
+    s->top = -1;
+}
+
+int isFull(Stack* s) {
+    return s->top == MAX - 1;
+}
+
+int isEmpty(Stack* s) {
+    return s->top == -1;
+}
+
+void push(Stack* s, int item) {
+    if (!isFull(s)) {
+        s->items[++(s->top)] = item;
+    } else {
+        printf("Стек переполнен\n");
+    }
+}
+
+int pop(Stack* s) {
+    if (!isEmpty(s)) {
+        return s->items[(s->top)--];
+    } else {
+        printf("Стек пуст\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+int evaluatePostfix(char* expression) {
+    Stack stack;
+    initStack(&stack);
+    char* token = strtok(expression, " ");
+
+    while (token != NULL) {
+        if (isdigit(token[0])) {
+            push(&stack, atoi(token));
+        } else {
+            int rightOperand = pop(&stack);
+            int leftOperand = pop(&stack);
+            switch (token[0]) {
+                case '+':
+                    push(&stack, leftOperand + rightOperand);
+                    break;
+                case '-':
+                    push(&stack, leftOperand - rightOperand);
+                    break;
+                case '*':
+                    push(&stack, leftOperand * rightOperand);
+                    break;
+                case '/':
+                    if (rightOperand == 0) {
+                        printf("Ошибка: Деление на ноль\n");
+                        exit(EXIT_FAILURE);
+                    }
+                    push(&stack, leftOperand / rightOperand);
+                    break;
+                default:
+                    printf("Недопустимый оператор: %s\n", token);
+                    exit(EXIT_FAILURE);
+            }
+        }
+        token = strtok(NULL, " ");
+    }
+    return pop(&stack);
+}
+
+int main() {
+    FILE* file = fopen("input.txt", "r");
+    if (file == NULL) {
+        printf("Ошибка открытия файла\n");
+        return 1;
+    }
+
+    char expression[256];
+    while (fgets(expression, sizeof(expression), file)) {
+        printf("Результат выражения: %d\n", evaluatePostfix(expression));
+    }
+
+    fclose(file);
+    return 0;
+}
