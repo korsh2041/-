66acdeafa554410513e975975dc22b9b58b522b6
korsh2041 <akorshunov2005@gmail.com>
2024-10-08 08:19:58 +0300

    Update task7.c

--- labs/lab2/task7.c
+++ labs/lab2/task7.c
@@ -1,282 +1,91 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-
-#define MAX 100
+#define n 128
 
 typedef struct {
-    int items[MAX];
+    int data[n];
     int top;
 } Stack;
 
-void initStack(Stack* s) {
-    s->top = -1;
-}
-
-int isFull(Stack* s) {
-    return s->top == MAX - 1;
-}
-
-int isEmpty(Stack* s) {
-    return s->top == -1;
-}
-
-void push(Stack* s, int item) {
-    if (!isFull(s)) {
-        s->items[++(s->top)] = item;
-    } else {
-        printf("Стек переполнен\n");
+void full(Stack *stack, int value) {
+    if (stack->top == n - 1) {
+        printf("Ошибка: стек переполнен\n");
+        exit(1);
     }
+    stack->data[++stack->top] = value;
 }
 
-int pop(Stack* s) {
-    if (!isEmpty(s)) {
-        return s->items[(s->top)--];
-    } else {
-        printf("Стек пуст\n");
-        exit(EXIT_FAILURE);
+int pust(Stack *stack) {
+    if (stack->top == -1) {
+        printf("Ошибка: стек пуст\n");
+        exit(1);
     }
+    return stack->data[stack->top--];
 }
 
-int evaluatePostfix(char* expression) {
+int perevod(const char *str) {
     Stack stack;
-    initStack(&stack);
-    char* token = strtok(expression, " ");
+    stack.top = -1;
 
-    while (token != NULL) {
-        if (isdigit(token[0])) {
-            push(&stack, atoi(token));
-        } else {
-            int rightOperand = pop(&stack);
-            int leftOperand = pop(&stack);
-            switch (token[0]) {
-                case '+':
-                    push(&stack, leftOperand + rightOperand);
-                    break;
-                case '-':
-                    push(&stack, leftOperand - rightOperand);
-                    break;
-                case '*':
-                    push(&stack, leftOperand * rightOperand);
-                    break;
-                case '/':
-                    if (rightOperand == 0) {
-                        printf("Ошибка: Деление на ноль\n");
-                        exit(EXIT_FAILURE);
+    for (int i = 0; str[i] != '\0'; i++) {
+        char ch = str[i];
+        if (ch == ' ') {
+            continue;
+        }
+
+        if (isdigit(ch)) {
+            int sch = 0;
+            while (isdigit(str[i])) {
+                sch = sch * 10 + (str[i] - '0');
+                i++;
+            }
+            i--;
+            full(&stack, sch);
+        }
+        else if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
+            int z2 = pust(&stack);
+            int z1 = pust(&stack);
+            int result;
+
+            switch (ch) {
+                case '+': 
+                result = z1 + z2; 
+                break;
+                case '-': 
+                result = z1 - z2; 
+                break;
+                case '*': 
+                result = z1 * z2; 
+                break;
+                case '/': 
+                    if (z2 == 0) {
+                        printf("Ошибка: деление на ноль\n");
+                        exit(1);
                     }
-                    push(&stack, leftOperand / rightOperand);
+                    result = z1 / z2; 
                     break;
                 default:
-                    printf("Недопустимый оператор: %s\n", token);
-                    exit(EXIT_FAILURE);
+                    printf("Ошибка: неизвестный оператор '%c'\n", ch);
+                    exit(1);
             }
-        }
-        token = strtok(NULL, " ");
-    }
-    return pop(&stack);
-}
-
-int main() {
-    FILE* file = fopen("input.txt", "r");
-    if (file == NULL) {
-        printf("Ошибка открытия файла\n");
-        return 1;
-    }
-
-    char expression[256];
-    while (fgets(expression, sizeof(expression), file)) {
-        printf("Результат выражения: %d\n", evaluatePostfix(expression));
-    }
-
-    fclose(file);
-    return 0;
-}
-
-
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-
-#define N 256
-
-typedef struct {
-    int items[N];
-    int top;
-} Stack;
-
-void inStack(Stack* s) {
-    s->top = -1;
-}
-
-int full(Stack* s) {
-    return s->top == N - 1;
-}
-
-int pust(Stack* s) {
-    return s->top == -1;
-}
-
-void stackfull(Stack* s, int sch) {
-    if (!full(s)) {
-        s->items[++(s->top)] = sch;
-    } else {
-        printf("Стек переполнен\n");
-    }
-}
-
-int stackpust(Stack* s) {
-    if (!pust(s)) {
-        return s->items[(s->top)--];
-    } else {
-        printf("Стек пуст\n");
-        exit(EXIT_FAILURE);
-    }
-}
-
-int preobraz(char* str) {
-    Stack stack;
-    inStack(&stack);
-    char *text = strtok(atoi(str), " ");
-
-    while (text != NULL) {
-        if (isdigit(text[0])) {
-            stackfull(&stack, atoi(text));
+            full(&stack, result);
         } else {
-            int prav = stackpust(&stack);
-            int levo = stackpust(&stack);
-            switch (text[0]) {
-                case '+':
-                    stackfull(&stack, levo + prav);
-                    break;
-                case '-':
-                    stackfull(&stack, levo - prav);
-                    break;
-                case '*':
-                    stackfull(&stack, levo * prav);
-                    break;
-                case '/':
-                    if (prav == 0) {
-                        printf("Ошибка: Деление на ноль\n");
-                        exit(EXIT_FAILURE);
-                    }
-                    stackfull(&stack, levo / prav);
-                    break;
-                default:
-                    printf("Недопустимый оператор: %s\n", text);
-                    exit(EXIT_FAILURE);
-            }
+            printf("Ошибка: недопустимый символ '%c'\n", ch);
+            exit(1);
         }
-        text =(int *) strtok(NULL, " ");
     }
-    return stackpust(&stack);
+    return pust(&stack);
 }
 
 int main() {
-    FILE* file = fopen("input.txt", "r");
+    FILE *file = fopen("input.txt", "r");
 
-    char str[N];
+    char str[128];
     while (fgets(str, sizeof(str), file)) {
-        printf("Результат выражения: %d\n", preobraz(str));
+        int result = perevod(str);
+        printf("Результат: %d\n", result);
     }
-
-    fclose(file);
-    return 0;
-}
-
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
-
-#define N 256
-
-typedef struct {
-    int items[N];
-    int top;
-} Stack;
-
-void inStack(Stack *s) {
-    s->top = -1;
-}
-
-int full(Stack *s) {
-    return s->top == N - 1;
-}
-
-int pust(Stack *s) {
-    return s->top == -1;
-}
-
-void stackfull(Stack *s, int sch) {
-    if (!full(s)) {
-        s->items[++(s->top)] = sch;
-    } else {
-        printf("Стек переполнен\n");
-    }
-}
-
-int stackpust(Stack *s) {
-    if (!pust(s)) {
-        return s->items[(s->top)--];
-    } else {
-        printf("Стек пуст\n");
-        exit(EXIT_FAILURE);
-    }
-}
-
-int preobraz(char *str) {
-    Stack stack;
-    inStack(&stack);
-    
-    char *text = strtok(str, " ");
-    
-    
-    while (text != NULL) {
-        for(int i = 0; text[i]; i++){
-            if (isdigit(text[i])) {
-                stackfull(&stack, atoi(text));
-            } else {
-                int prav = stackpust(&stack);
-                int levo = stackpust(&stack);
-                switch (text[i]) {
-                    case '+':
-                        stackfull(&stack, levo + prav);
-                        break;
-                    case '-':
-                        stackfull(&stack, levo - prav);
-                        break;
-                    case '*':
-                        stackfull(&stack, levo * prav);
-                        break;
-                    case '/':
-                        if (prav == 0) {
-                            printf("Ошибка: Деление на ноль\n");
-                            exit(EXIT_FAILURE);
-                        }
-                        stackfull(&stack, levo / prav);
-                        break;
-                    default:
-                        printf("Недопустимый оператор: %s\n", text);
-                        exit(EXIT_FAILURE);
-                }
-            }
-            text = strtok(NULL, " ");
-        }
-    }
-    
-    return stackpust(&stack);
-}
-
-int main() {
-    FILE* file = fopen("input.txt", "r");
-
-    char str[N];
-    while (fgets(str, sizeof(str), file)) {
-        printf("Результат выражения: %d\n", preobraz(str));
-    }
-
     fclose(file);
     return 0;
 }
